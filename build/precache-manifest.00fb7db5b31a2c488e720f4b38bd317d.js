4599cf8c73a864729aa7047bdeafa9245763a611
rizwan230698 <rizwananwar230698@gmail.com>
2020-01-07 11:37:10 +0530

    adding files

---
+++ build/precache-manifest.00fb7db5b31a2c488e720f4b38bd317d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2286675fdaf07a9b288c30ec5d117c51",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e40d123070b82ee2a3d2",
+    "url": "/monsters-rolodex/static/css/main.70a75f5d.chunk.css"
+  },
+  {
+    "revision": "88f7a088e2a5c5ad4db2",
+    "url": "/monsters-rolodex/static/js/2.bcfb89b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.bcfb89b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e40d123070b82ee2a3d2",
+    "url": "/monsters-rolodex/static/js/main.5aa67f0f.chunk.js"
+  },
+  {
+    "revision": "8e0631e8fb00ca5a6969",
+    "url": "/monsters-rolodex/static/js/runtime-main.41315a6c.js"
+  }
+]);
